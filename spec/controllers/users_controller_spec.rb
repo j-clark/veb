27db1798f61814f270d2cee3197101f7b5853c2c
Joshua Clark <joshua.clark@txstate.edu>
2014-01-07 23:49:19 -0500

    refactors users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 
 describe UsersController do
   describe "#create" do
-    before { post :create, username: 'josh', email: 'atxjclark@gmail.com' }
+    let(:email) { 'atxjclark@gmail.com' }
+
+    before { post :create, username: 'josh', email: email }
 
     it 'is successful' do
       expect(response).to be_ok
     end
 
     it 'creates a user' do
-      expect(User.find_by(email: 'atxjclark@gmail.com')).to be
+      expect(User.find_by(email: email)).to be
     end
   end
 end
